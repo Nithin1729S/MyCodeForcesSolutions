744e936a0f2de82e808da9b78e5e1c5a265bb884
nithin <sureshnithin1729@gmail.com>
2024-03-05 21:52:59 +0530

    5th March 2024

---
+++ 39_GeorgeAccomodation.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define vi vector<int>
+#define vll vector<long long int>
+#define fr first
+#define sc second
+#define INF LLONG_MAX
+#define MOD 1000000007
+#define pii pair<int,int>
+#define int long long
+#define endl '\n'
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define clr(x) memset(x,0,sizeof(x))
+#define sortUni(v) sort(all(v)), v.erase(unique(all(v)), v.end())
+#define fast_io ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define f0(i,n)  for(int i=0;i<(int)n;i++)
+#define f1(i,n)  for(int i=1;i<=(int)n;i++)
+#define test int t;cin>>t;while(t--)
+#define take(a,n) vi a(n); f0(i,n) cin >> a[i];
+#define give(a,n) f0(i,n){cout << a[i] << ' ';}cout << endl;
+
+void solve()
+{
+    int res=0;
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int a,b;
+        cin>>a>>b;
+        if(abs(a-b)>=2) res++;
+    }   
+    cout<<res<<endl;
+}
+int32_t main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("in.txt","r",stdin);
+        freopen("out.txt","w",stdout);
+    #endif
+    fast_io;
+    //test
+        solve();
+    return 0;
+}
+
